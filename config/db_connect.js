fa025bc769e02eba93dde07a7ddf994970fabb62
Mohamed Safras <awsafras@gmail.com>
2023-02-26 06:54:22 +0530

    database connection

---
+++ config/db_connect.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+const dbConnect = async () => {
+  await mongoose.connect(
+    process.env.DB_CONNECTION_URL,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    },
+    () => {
+      console.log("Database connection is successfully established");
+    }
+  );
+};
+
+module.exports = dbConnect;
